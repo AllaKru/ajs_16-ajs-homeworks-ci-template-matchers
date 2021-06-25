aafd8da0ab73a2d0d5ebbbc2cb7b3cb01a55b429
Alla Kruglova <renata-l@yandex.ru>
2021-06-26 00:55:40 +0300

    not.toBe

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -16,3 +16,19 @@ test('the order must be sorted from highest to lowest', () => {
 
   expect(received).toEqual(expected);
 });
+
+test('the order must be sorted from highest to lowest', () => {
+  const received = heroes([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  const expected = heroes([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+
+  expect(received).not.toBe(expected);
+});
